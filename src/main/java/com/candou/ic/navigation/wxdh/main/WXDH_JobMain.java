42e3a3837703da2dad918986b7c9fd429360a216
jiangchao1987 <jiangchao1987@gmail.com>
2013-05-29 10:39:44 +0800

    navigation added.
    Signed-off-by: jiangchao1987 <jiangchao1987@gmail.com>

---
+++ src/main/java/com/candou/ic/navigation/wxdh/main/WXDH_JobMain.java
@@ -0,0 +1,13 @@
+package com.candou.ic.navigation.wxdh.main;
+
+public class WXDH_JobMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
