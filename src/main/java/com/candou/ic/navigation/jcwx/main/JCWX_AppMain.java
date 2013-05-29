3ad79f95f2f28c2af93a81251da9e266531fbb82
jiangchao1987 <jiangchao1987@gmail.com>
2013-05-29 10:54:44 +0800

    dev-liweiniu branch added.
    Signed-off-by: jiangchao1987 <jiangchao1987@gmail.com>

---
+++ src/main/java/com/candou/ic/navigation/jcwx/main/JCWX_AppMain.java
@@ -0,0 +1,13 @@
+package com.candou.ic.navigation.jcwx.main;
+
+public class JCWX_AppMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
