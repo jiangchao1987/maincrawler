86303ba4750ebf072b0660563288f5381c15fc97
niuliwei <liweiniu@candou.com>
2013-05-30 17:13:09 +0800

    create db sql.

--- res/sql/jcwx/jcwx.sql
+++ res/sql/jcwx/jcwx.sql
@@ -30,3 +30,41 @@ CREATE TABLE `jcwx_article` (
   KEY `created_at` (`created_at`),
   KEY `updated_at` (`updated_at`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `jcwx_like` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `like_id` int(11) NOT NULL DEFAULT '0' COMMENT '猜你喜欢id',
+  `title` varchar(255) NOT NULL COMMENT '猜你喜欢的名称',
+  `thumbnail` varchar(255) DEFAULT NULL COMMENT '猜你喜欢的图片路径',
+  `article_id` int(11) NOT NULL COMMENT '关联jcwx_article的article_id',
+  `created_at` varchar(255) NOT NULL,
+  `updated_at` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `like_id` (`like_id`),
+  UNIQUE KEY `article_id` (`article_id`),
+  KEY `title` (`title`),
+  KEY `created_at` (`created_at`),
+  KEY `updated_at` (`updated_at`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `jcwx_job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_id` int(11) DEFAULT NULL COMMENT '任务id（访问页面的id）',
+  `title` varchar(255) DEFAULT NULL COMMENT '工作名称',
+  `cid` int(11) DEFAULT NULL COMMENT '所属类别id',
+  `cname` varchar(255) DEFAULT NULL COMMENT '类别名称',
+  `views` int(11) DEFAULT NULL COMMENT '关注人数',
+  `wxh` varchar(255) DEFAULT NULL COMMENT '微信号',
+  `content` text COMMENT '微信介绍',
+  `thumbnail` varchar(255) DEFAULT NULL COMMENT '头像图片路径',
+  `created_at` varchar(255) DEFAULT NULL COMMENT '创建时间',
+  `updated_at` varchar(255) DEFAULT NULL COMMENT '修改时间',
+  `is_matched` int(11) DEFAULT '0' COMMENT '是否是待抓取',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `job_id` (`job_id`),
+  KEY `title` (`title`),
+  KEY `cid` (`cid`),
+  KEY `cname` (`cname`),
+  KEY `created_at` (`created_at`),
+  KEY `updated_at` (`updated_at`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ res/sql/wxdh/wxdh.sql
@@ -0,0 +1,64 @@
+CREATE TABLE `wxdh_app` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(20) DEFAULT NULL COMMENT '名称',
+  `intro` varchar(200) DEFAULT NULL COMMENT '简介',
+  `url` varchar(100) DEFAULT NULL COMMENT '微信关注跳转地址',
+  `f` int(11) DEFAULT NULL,
+  `oc` varchar(20) DEFAULT NULL COMMENT '信微号',
+  `wsu` varchar(100) DEFAULT NULL,
+  `detail` varchar(20) DEFAULT NULL,
+  `dts` int(11) DEFAULT NULL COMMENT '能可是加入时间',
+  `cid` int(11) DEFAULT NULL,
+  `cname` varchar(20) DEFAULT NULL COMMENT '分类名称',
+  `icon` varchar(100) DEFAULT NULL COMMENT '头像',
+  `icon_name` varchar(100) DEFAULT NULL,
+  `imc` varchar(100) DEFAULT NULL COMMENT '二维码地址',
+  `imc_name` varchar(100) DEFAULT NULL,
+  `sc` int(11) DEFAULT NULL,
+  `direct_number` int(11) DEFAULT NULL COMMENT '关注人数',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`),
+  KEY `created_at` (`created_at`),
+  KEY `updated_at` (`updated_at`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `wxdh_category` (
+  `cid` int(11) NOT NULL DEFAULT '0' COMMENT '分类ID',
+  `cname` varchar(20) DEFAULT NULL COMMENT '分类名称',
+  PRIMARY KEY (`cid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `wxdh_job` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(20) DEFAULT NULL COMMENT '名称',
+  `intro` varchar(200) DEFAULT NULL COMMENT '简介',
+  `url` varchar(100) DEFAULT NULL COMMENT '微信关注跳转地址',
+  `f` int(11) DEFAULT NULL,
+  `oc` varchar(20) DEFAULT NULL COMMENT '信微号',
+  `cid` int(11) DEFAULT NULL,
+  `cname` varchar(20) DEFAULT NULL COMMENT '分类名称',
+  `icon` varchar(100) DEFAULT NULL COMMENT '头像',
+  `icon_name` varchar(100) DEFAULT NULL,
+  `direct_number` int(11) DEFAULT NULL COMMENT '关注人数',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `is_matched` int(11) DEFAULT '0' COMMENT '是否已经抓取',
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`),
+  KEY `cid` (`cid`),
+  KEY `created_at` (`created_at`),
+  KEY `updated_at` (`updated_at`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `wxdh_photo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `appid` int(11) DEFAULT NULL,
+  `photo_url` varchar(100) DEFAULT NULL,
+  `type` int(11) DEFAULT NULL,
+  `filename` varchar(100) DEFAULT NULL COMMENT '图片文件名称',
+  PRIMARY KEY (`id`),
+  KEY `appid_index1` (`appid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
