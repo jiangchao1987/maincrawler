2da15608ac5c04f83803127698b878b074143cb8
jiangchao1987 <jiangchao1987@gmail.com>
2013-05-31 15:17:04 +0800

    wxdh sql modified.
    Signed-off-by: jiangchao1987 <jiangchao1987@gmail.com>

--- res/sql/wxdh/wxdh.sql
+++ res/sql/wxdh/wxdh.sql
@@ -21,7 +21,9 @@ CREATE TABLE `wxdh_app` (
   PRIMARY KEY (`id`),
   KEY `name` (`name`),
   KEY `created_at` (`created_at`),
-  KEY `updated_at` (`updated_at`)
+  KEY `updated_at` (`updated_at`),
+  KEY `cid` (`cid`),
+  KEY `cname` (`cname`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `wxdh_category` (
@@ -39,8 +41,6 @@ CREATE TABLE `wxdh_job` (
   `oc` varchar(20) DEFAULT NULL COMMENT '信微号',
   `cid` int(11) DEFAULT NULL,
   `cname` varchar(20) DEFAULT NULL COMMENT '分类名称',
-  `icon` varchar(100) DEFAULT NULL COMMENT '头像',
-  `icon_name` varchar(100) DEFAULT NULL,
   `direct_number` int(11) DEFAULT NULL COMMENT '关注人数',
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
@@ -57,8 +57,12 @@ CREATE TABLE `wxdh_photo` (
   `appid` int(11) DEFAULT NULL,
   `photo_url` varchar(100) DEFAULT NULL,
   `type` int(11) DEFAULT NULL,
-  `filename` varchar(100) DEFAULT NULL COMMENT '图片文件名称',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `appid_index1` (`appid`)
+  UNIQUE KEY `appid_photourl` (`appid`,`photo_url`),
+  KEY `app_id` (`appid`),
+  KEY `created_at` (`created_at`),
+  KEY `updated_at` (`updated_at`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
